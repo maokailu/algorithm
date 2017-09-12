936d94b3fa98e1f8f452b9c7b781ff5948cffc40
maokailu <1113901189@qq.com>
2017-09-12 20:21:42 +0800

    数组重复元素提取

---
+++ array-distinct/distinct.js
@@ -0,0 +1,18 @@
+Array.prototype.distinct = function() {
+    var ret = [];
+    for (var i = 0; i < this.length; i++)
+    {
+        // 遍历数组中每个元素
+        for (var j = i+1; j < this.length;) { 
+            // 将当前i元素与其后的元素进行比较
+            if (this[i] === this[j]) {
+                // 删除j返回被删除的元素数组
+                ret.push(this.splice(j, 1)[0]);
+            } else {
+                j++;
+            }
+        }
+    }
+    return ret;
+}
+console.log(['a','b','c','d','b','a','e'].distinct());

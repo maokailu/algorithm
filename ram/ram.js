4764470a290794a2a3f5d9f447161b3054ed5b3a
maokailu <1113901189@qq.com>
2017-09-12 14:39:36 +0800

    Random Access machine

---
+++ ram/ram.js
@@ -0,0 +1,18 @@
+// 功能：向下取整的除法 c/d
+// [c/d] = max{x | dx <= c}
+//     = max{x | dx < 1 + c }
+// 算法：反复地从R[0] = 1 + c 中减去R[1]=d
+// 统计在下溢之前，所做减法的次数
+function Floor(){
+    var R =[7, 2, 0, 1];
+    R[0] = R[0] + R[3]; //c++
+    
+    while(R[0] > 0){
+        R[0] = R[0] - R[1]; //c-=d
+        R[2] = R[2] + R[3]; //x++
+    }
+    
+    R[0] = R[2] - R[3] //else x-- and
+    console.log(R[0]); // return R[0] = x =[c/d]
+}
+window.onload = Floor;

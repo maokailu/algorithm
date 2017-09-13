95d50508c5a03958e716ec12da91f2b8385c522e
maokailu <1113901189@qq.com>
2017-09-13 15:38:45 +0800

    格式

--- ram/ram.js
+++ ram/ram.js
@@ -3,7 +3,7 @@
 //     = max{x | dx < 1 + c }
 // 算法：反复地从R[0] = 1 + c 中减去R[1]=d
 // 统计在下溢之前，所做减法的次数
-function Floor(){
+function floor(){
     var R =[7, 2, 0, 1];
     R[0] = R[0] + R[3]; //c++
     
@@ -15,4 +15,4 @@ function Floor(){
     R[0] = R[2] - R[3] //else x-- and
     console.log(R[0]); // return R[0] = x =[c/d]
 }
-window.onload = Floor;
+window.onload = floor;

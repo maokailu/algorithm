e8f78f866bad28a8b546964a9a7376790df2c44e
maokailu <1113901189@qq.com>
2017-09-17 19:56:49 +0800

    用递归实现数组倒置

---
+++ iterationAndRecursion/index.js
@@ -0,0 +1,13 @@
+// 数组倒置
+var a = [1,2,3,7,3];
+var lo =0,hi=a.length-1;
+function reverse(a, lo, hi) {
+    if(lo<hi){
+        var temp = a[hi];
+        a[hi] = a[lo];
+        a[lo] = temp;
+        reverse(a, lo+1, hi-1);
+    }else return;
+}
+window.onload = reverse(a, lo, hi);//传全局怎么办
+console.log(a);

32bddad30998107832f4c0328e2609f24dec9a8a
maokailu <1113901189@qq.com>
2017-09-18 21:27:16 +0800

    添加无序向量唯一化、删除等算法

--- vector/unorderVector.js
+++ vector/unorderVector.js
@@ -1,18 +1,3 @@
-//有序向量
-//唯一化
-var v = [1,1,1,1,2,2,2,3,3,4,4,4];
-console.log('有序数组v：' + v);
-function uniquify(){
-    var i =0,j=0;
-    while (++j<v.length){
-        if(v[i]!=v[j]){
-            v[++i]=v[j];
-        }
-    }
-    v.length = i++;//直接截取尾部多余元素
-    return j-i;//向量规模变化量，即被删除元素总数
-}
-console.log('有序数组v唯一化：' + uniquify());
 
 //无序向量
 // 插入
